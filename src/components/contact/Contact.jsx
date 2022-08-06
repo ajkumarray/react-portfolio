b8add4602a92d53b09aa285a105d2d22b56ea537
ajkumarrayoff@gmail.com <ajkumarrayoff@gmail.com>
2022-08-07 00:57:21 +0530

    modified Contact and Footer section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
+import emailjs from "emailjs-com";
 import { MdEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri";
 import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_awue2fq",
+        "template_ny6wyzm",
+        form.current,
+        "loaHdYSyx6bluQPkH"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -46,7 +70,7 @@ const Contact = () => {
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input
             type="text"
             name="name"
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        AJIT KUMAR
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a
+          href="https://facebook.com/ajkumarray"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://instagram.com/ajkumarray"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://twitter.com/ajkumarray"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; AJIT KUMAR All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
